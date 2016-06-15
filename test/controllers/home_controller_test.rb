08436802a58ccdd6612e330ec301ef0ac54f2a9f
Gaurav <gaurav5810@gmail.com>
2016-06-16 00:28:56 +0530

    test cases for html input

--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -11,21 +11,28 @@ class HomeControllerTest < ActionController::TestCase
   end
 
   test "should give no file error if no file uploaded" do
-    post :upload
+    post :upload, {:input_type => "file"}
     assert response.body.include?(NO_FILE_PROVIDED)
   end
 
   test "should give error if file format is incorrect" do
     wrong_file = fixture_file_upload('lib/input_wrong.txt', 'text/xml')
-    post :upload, :input_data => wrong_file
+    post :upload, {:input_data => wrong_file, :input_type => "file" }
     assert response.body.include?(ERROR_READING_FILE)
   end
 
-  test "should give correct result for valid input" do
-    post :upload, :form_data => @file
+  test "should give correct result for valid file input" do
+    post :upload, {:form_data => @file, :input_type => "file" }
     assert response.body.include?("7")
   end
 
-  
+  test "should give error if wrong input provided" do
+    post :upload, {:low_ch=>"sds", :high_ch=>"dssd", :input_type=>"html"}
+    assert response.body.include?(INCORRECT_DATA)
+  end
 
+  test "should give correct result for valid html input" do
+    post :upload, {:low_ch=>1, :high_ch=>20, :blocked_list => "18 19", :view_sequence => "15 14 17 1 17", :input_type=>"html"}
+    assert response.body.include?("7")
+  end
 end
