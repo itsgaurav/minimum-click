7af07980a2ca9290e849cd46c25cf8cee1721a6b
Gaurav <gaurav5810@gmail.com>
2016-06-15 22:37:02 +0530

    write test cases for app

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,23 +1,50 @@
+require 'constant'
 class HomeController < ApplicationController
-  before_filter :valid_input, :only => :upload
+  before_filter :validate_input, :only => :upload
 
   def index
   end
 
   # Reads the input file and prints solution
   def upload
-    solve(@viewable_list,@browse_list)
-    render :result
+    begin
+      solve(@viewable_list,@browse_list)
+      render :result
+    rescue Exception => e
+      raise_error INCORRECT_DATA
+    end
   end
 
   private
-  # Checkk if input present and valid
-  def valid_input
-		if params[:input_data].present?
+  # Checkk if input valid
+  def validate_input
+    if params[:input_type]=="file"
+      validate_file_input
+    else
+      validate_html_input
+    end
+  end
+
+  # Validates the file input
+  def validate_file_input
+    if params[:input_data].present?
       check_format(params[:input_data])
     else
-			raise_error "INCORRECT FILE CONTENT"
+			raise_error NO_FILE_PROVIDED
 		end
+  end
+
+  # Validates the file input
+  def validate_html_input
+    begin
+      l = params[:low_ch].to_i
+      h = params[:high_ch].to_i
+      channel_list = (l..h).to_a  
+      blocked_list = params[:blocked_list].split(" ").map { |e| e.to_i }
+      @viewable_list = channel_list - blocked_list
+      @browse_list = params[:view_sequence].split(" ").map { |e| e.to_i }
+    rescue Exception => e
+    end
   end
 
   # Helper method to check file format
@@ -27,40 +54,44 @@ class HomeController < ApplicationController
       content = content.split("\n").reject { |c| c.empty? }
       read_file(content)
     rescue
-      raise_error "ERROR READING FILE"
+      raise_error ERROR_READING_FILE
     end
   end
 
   def file_content(file)
-  	if file.respond_to?(:read)
+    if file.respond_to?(:read)
   		content = file.read
 		elsif file.respond_to?(:path)
-  		content = File.read(file_data.path)
+  		content = File.read(file.path)
   	else
-  		raise_error 
+  		content ""
   	end
   	content
   end
 
   def raise_error(error)
-  	flash.now[:notice] = error
+    flash.now[:notice] = error
 		render :index and return
   end
 
   def read_file(lines)
-    raise_error and return if lines.count!=3
+    raise ERROR_READING_FILE and return if lines.count!=3
     temp = lines[0].split(" ") # Read start and end channel
-    raise_error and return if temp.count!=2
+    raise ERROR_READING_FILE and return if temp.count!=2
     channel_list = (temp[0].to_i..temp[1].to_i).to_a # channel arrays
     temp = lines[1].split(" ") # Blocked List
     blocked_list_count = temp[0].to_i
-    blocked_list = temp[1..-1].map { |e| e.to_i }
-    raise_error and return if blocked_list.count!=blocked_list_count
+    if blocked_list_count==0
+      blocked_list = []
+    else
+      blocked_list = temp[1..-1].map { |e| e.to_i }
+    end
+    raise ERROR_READING_FILE and return if blocked_list.count!=blocked_list_count
     @viewable_list = channel_list - blocked_list
     temp = lines[2].split(" ") # Blocked List
     browse_list_count = temp[0].to_i
     @browse_list = temp[1..-1].map { |e| e.to_i }
-    raise_error and return if @browse_list.count!=browse_list_count
+    raise ERROR_READING_FILE and return if @browse_list.count!=browse_list_count
   end
 
   def solve(view_list,browse_list)
--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -1,9 +1,31 @@
 require 'test_helper'
-
+require 'constant'
 class HomeControllerTest < ActionController::TestCase
+  def setup
+    @file = fixture_file_upload('lib/input.txt', 'text/xml')
+  end
+
   test "should get index" do
     get :index
     assert_response :success
   end
+
+  test "should give no file error if no file uploaded" do
+    post :upload
+    assert response.body.include?(NO_FILE_PROVIDED)
+  end
+
+  test "should give error if file format is incorrect" do
+    wrong_file = fixture_file_upload('lib/input_wrong.txt', 'text/xml')
+    post :upload, :input_data => wrong_file
+    assert response.body.include?(ERROR_READING_FILE)
+  end
+
+  test "should give correct result for valid input" do
+    post :upload, :form_data => @file
+    assert response.body.include?("7")
+  end
+
+  
 
 end
